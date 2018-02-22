611e70f621a9b9b7add4c6cd68e79c7b5cc251b8
lujiamin <1207198860@qq.com>
2018-02-22 20:48:07 +0800

    2018.2.22

--- src/com/wangdao/output/Trapezoid.java
+++ src/com/wangdao/output/Trapezoid.java
@@ -1,4 +1,4 @@
-package Output;
+package com.wangdao.output;
 
 import java.util.Scanner;
 
@@ -7,19 +7,20 @@ import java.util.Scanner;
  */
 public class Trapezoid {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int h = scanner.nextInt();
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < h + 2 * (h - 1); j++) {
-				if (j < 2 * h - 2 * i - 2) {
-					System.out.print(" ");
-				} else {
-					System.out.print("*");
-				}
-			}
-			System.out.println();
-		}
-		scanner.close();
-	}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int h = scanner.nextInt();
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < h + 2 * (h - 1); j++) {
+                if (j < 2 * h - 2 * i - 2) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
