14cf8f2738cfa0c6a8a2ab994a350f27e8bd524b
lujiamin <1207198860@qq.com>
2018-02-26 00:28:02 +0800

    2018.2.25

--- src/com/wangdao/math/BigNum.java
+++ src/com/wangdao/math/BigNum.java
@@ -9,6 +9,9 @@ public class BigNum {
         BigInteger a = new BigInteger("20");
         System.out.println(pow(a, 5));
         System.out.println(factorial(a));
+        String str1 = "13456";
+        String str2 = "456789";
+        System.out.println(add(str1, str2));
     }
 
     // 大数求幂
@@ -30,5 +33,56 @@ public class BigNum {
         }
         return ans;
     }
+
+    // 使用字符串存储的大数加法
+    public static String add(String a, String b) {
+        int l1 = a.length();
+        int l2 = b.length();
+        int[] arr1 = new int[l1 + 1];
+        int[] arr2 = new int[l2 + 1];
+        int[] ans = new int[Math.max(l1, l2) + 1];
+        int i = 0, j;
+        for (j = l1 - 1; j >= 0; j--) {
+            arr1[i++] = a.charAt(j) - '0';
+        }
+        i = 0;
+        for (j = l2 - 1; j >= 0; j--) {
+            arr2[i++] = b.charAt(j) - '0';
+        }
+        // 按位进行加法运算
+        i = 0;
+        int carry = 0;
+        while (l1 > 0 && l2 > 0) {
+            ans[i] = (arr1[i] + arr2[i] + carry) % 10;
+            carry = (arr1[i] + arr2[i] + carry) / 10;
+            i++;
+            l1--;
+            l2--;
+        }
+        while (l1 > 0) {
+            ans[i] = (arr1[i] + carry) % 10;
+            carry = (arr1[i] + carry) / 10;
+            i++;
+            l1--;
+        }
+        while (l2 > 0) {
+            ans[i] = (arr2[i] + carry) % 10;
+            carry = (arr2[i] + carry) / 10;
+            i++;
+            l2--;
+        }
+        i--;
+        // 最高位有进位
+        if (carry > 0) {
+            ans[++i] = carry;
+        }
+        // 返回结果字符串
+        String str = "";
+        while (i >= 0) {
+            str += ans[i--];
+        }
+        return str;
+    }
+
 
 }
--- src/com/wangdao/math/Last3BitsOfPow.java
+++ src/com/wangdao/math/Last3BitsOfPow.java
@@ -3,12 +3,24 @@ package com.wangdao.math;
 public class Last3BitsOfPow {
 
     public static void main(String[] args) {
-
+        int a = 2;
+        int b = 31;
+        System.out.println(pow(a, b));
     }
 
-    // 返回a^b结果的后3位
+    // 返回a^b结果的后3位，二分求幂(b写成二进制形式,31:11111)
     public static int pow(int a, int b) {
-        return 0;
+        int ans = 1;
+        while (b != 0) {
+            // 当前二进制位为1
+            if (b % 2 == 1) {
+                ans *= a;
+                ans %= 1000; // 取结果后三位
+            }
+            b /= 2;
+            a *= a;
+            a %= 1000; // 取a的后三位
+        }
+        return ans;
     }
-
 }
