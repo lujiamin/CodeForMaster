291ab93c63927e19b0c3763f47ec637313a64832
lujiamin <1207198860@qq.com>
2018-03-04 14:14:01 +0800

    2018.3.4

--- src/com/wangdao/stack/Calculator.java
+++ src/com/wangdao/stack/Calculator.java
@@ -10,16 +10,87 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String exp = scanner.nextLine();
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            if (str.equals("0")) {
+                break;
+            }
+            String elements[] = str.split(" ");
+            Stack<Character> operate = new Stack<>();
+            Stack<Double> operand = new Stack<>();
+            int i;
+            double result = 0;
+            for (i = 0; i < elements.length; i++) {
 
-        Stack<Double> operand = new Stack<Double>(); // 数字栈
-        Stack<Character> operator = new Stack<Character>(); // 运算符栈
+                if (elements[i].equals("+") || elements[i].equals("-")) {// 加号和减号的情况
+                    while (operand.size() > 1) {
+                        double b = operand.pop();
+                        double a = operand.pop();
+                        switch (operate.pop()) {
+                            case '+':
+                                result = a + b;
+                                break;
+                            case '-':
+                                result = a - b;
+                                break;
+                            case '*':
+                                result = a * b;
+                                break;
+                            case '/':
+                                result = a / b;
+                                break;
+                            default:
+                                break;
+                        }
+                        operand.push(result);
+                    }
+                    operate.push(elements[i].charAt(0)); // 转化成char类型
+                } else if (elements[i].equals("*") || elements[i].equals("/")) {// 乘号，除号的情况
+                    while (operand.size() > 1 && (operate.peek() == '*' || operate.peek() == '/')) {
+                        double b = operand.pop();
+                        double a = operand.pop();
+                        switch (operate.pop()) {
+                            case '*':
+                                result = a * b;
+                                break;
+                            case '/':
+                                result = a / b;
+                                break;
+                            default:
+                                break;
+                        }
+                        operand.push(result);
+                    }
+                    operate.push(elements[i].charAt(0));
+                } else {// 数值的情况
+                    operand.push(Double.parseDouble(elements[i]));
+                }
 
-        for (int i = 0; i < exp.length(); i++) {
-
+            }
+            while (!operate.isEmpty() && !operand.isEmpty()) {
+                double b = operand.pop();
+                double a = operand.pop();
+                switch (operate.pop()) {
+                    case '+':
+                        result = a + b;
+                        break;
+                    case '-':
+                        result = a - b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    case '/':
+                        result = a / b;
+                        break;
+                    default:
+                        break;
+                }
+                operand.push(result);
+            }
+            System.out.printf("%.2f\n", operand.peek());
         }
         scanner.close();
-
     }
 
 }
